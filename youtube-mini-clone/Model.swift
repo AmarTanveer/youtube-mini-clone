7e32142d600dd916f8f1fb9dbf703a5ec988f5c1
Amar Tanveer <amartanveer@gmail.com>
2024-05-08 13:21:37 +0530

    Added tableview, custom protocol in the model and displayed video titles in the TableView

--- youtube-mini-clone/Model.swift
+++ youtube-mini-clone/Model.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos: [Video])
+}
+
 
 class Model {
     
+    var delegate: ModelDelegate?
     
     func getVideos() {
         
@@ -39,6 +45,16 @@ class Model {
                 
                 let response = try decoder.decode(Response.self, from: data!)
                 
+                if response.items != nil {
+                    
+                    DispatchQueue.main.async {
+                        
+                        // Calling videosFetched method of the delegate
+                        self.delegate?.videosFetched(response.items!)
+                        
+                    }
+                        
+                }
                 
                 dump(response)
             }
--- youtube-mini-clone/ViewController.swift
+++ youtube-mini-clone/ViewController.swift
@@ -7,16 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+   
+    @IBOutlet weak var tableView: UITableView!
+    
     var model = Model()
+    var videos = [Video]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         model.getVideos()
+        tableView.dataSource = self
+        tableView.delegate = self
+        model.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
+    func videosFetched(_ videos: [Video]) {
+        // Set the returned videos to our property
+        self.videos = videos
+        
+        // Refresh the tableView
+        tableView.reloadData()
+    }
+    
+    
+    // MARK: - TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        // Configure the cell with data
+        let title = videos[indexPath.row].title
+        cell.textLabel?.text = title
+        
+        
+        return cell
+    }
+    
+    
 
 }
 
